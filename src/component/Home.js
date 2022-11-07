b7b727087035b383d09d3bb73422eab40f0379e6
Alpaca Uniix <supatsara_wan@cmu.ac.th>
2022-11-07 19:18:48 +0700

    add button

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { BsKeyFill } from 'react-icons/Bs';
+import { HiUsers } from 'react-icons/Hi';
+import { MdLibraryBooks } from 'react-icons/Md';
 
 export default function (ButtonGetToken){
     return(
         <>
-        
+        <Button variant="contained"> <BsKeyFill></BsKeyFill> Access Token</Button>
+        <Button variant="contained"> <HiUsers></HiUsers> Users</Button>
+        <Button variant="contained"> <MdLibraryBooks></MdLibraryBooks> Books</Button>
         </>
     )
 }
